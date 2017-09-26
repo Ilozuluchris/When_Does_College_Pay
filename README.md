c92a09a740e32aab7b9aa02ea0c6dc2b242c7512
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-09-26 23:56:04 +0100

    Some read me changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # When_Does_College_Pay
 This explores what college degree pays out in the end
+
+Data would analysed by region,type of college and type of degree
--- plot.py
+++ plot.py
@@ -1,12 +1,8 @@
 import numpy as np
 import pandas as pd
 
-region_data =  pd.read_csv('salaries-by-region.csv')
-
+region_data =  pd.read_csv('salaries-by-region.csv',keep_default_na=False)
 
 region_data_array = np.array(region_data)
 
-for data in region_data_array:
-    if data == n:
-        data = 0
-print region_data_array
+print region_data.head()
