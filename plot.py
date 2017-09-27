3df227aea16d0abd4aa5b621d23217a6f299bbb7
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-09-27 21:41:43 +0100

    Removed duplicates in data frame

--- plot.py
+++ plot.py
@@ -1,8 +1,4 @@
 import numpy as np
 import pandas as pd
 
-region_data =  pd.read_csv('salaries-by-region.csv').dropna()
-
-print region_data.head()
-
-print region_data.drop_duplicates()
+region_data =  pd.read_csv('salaries-by-region.csv').dropna().drop_duplicates()
