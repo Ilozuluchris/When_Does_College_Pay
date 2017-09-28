809484bad4cf9e81a22fc18de16c4307344e809d
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-09-28 23:06:23 +0100

    Abstracted importing and cleaning of csv

--- plot.py
+++ plot.py
@@ -1,4 +1,14 @@
 import numpy as np
 import pandas as pd
 
-region_data =  pd.read_csv('salaries-by-region.csv').dropna().drop_duplicates()
+def import_and_clean_csv(csv):
+    """
+    :param csv: string describing path to csv to be imported
+    :return: pandas data frame containing clean data.
+    """
+    return pd.read_csv(csv).dropna().drop_duplicates()
+
+
+region_data = import_and_clean_csv('salaries-by-region.csv')
+
+print region_data.head()
