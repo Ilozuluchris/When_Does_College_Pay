bce0670e768e70bb8c5c6425776b28a08dc5b734
ilozuluchris <ilozuluchidiuso@gmail.com>
2017-09-29 00:24:26 +0100

    cleareformatting and converting some columns to numeric values

--- plot.py
+++ plot.py
@@ -11,4 +11,14 @@ def import_and_clean_csv(csv):
 
 region_data = import_and_clean_csv('salaries-by-region.csv')
 
+#print region_data.head() # to validate the csv was imported
+
+cols = ["Starting Median Salary","Mid-Career Median Salary","Mid-Career 10th Percentile Salary","Mid-Career 25th Percentile Salary",
+"Mid-Career 75th Percentile Salary",
+"Mid-Career 90th Percentile Salary"]
+region_data[cols] =region_data[cols].applymap(lambda x: pd.to_numeric(x.replace("$", "").replace(",", ""))) # removes $ sign and commas then converts to numeric values
+'''
+to validate above operation worked
 print region_data.head()
+print region_data.dtypes
+'''
